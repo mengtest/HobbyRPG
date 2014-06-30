ed8eedfd6c319a1357e067e08707cf00295113e8
Gerald Wong <gerald.wong@tecmokoei.com.sg>
2014-06-30 14:50:39 +0800

    added needed battle functions to pkg

--- Classes/Battle/BattleEntities/BattleEntity.h
+++ Classes/Battle/BattleEntities/BattleEntity.h
@@ -17,6 +17,8 @@ public:
 	inline const cocos2d::CCPoint& getPosition() const { return m_sprite->getPosition(); }
 	inline const cocos2d::CCSprite * getSprite() { return m_sprite; }
 	inline const bool getIsEnemy() { return m_bIsEnemy; } 
+    
+    inline void setPosition(cocos2d::CCPoint& position) { m_sprite->setPosition(position); }
 
 	// careful with this
 	void addSpriteToLayer( cocos2d::CCLayer * layer );
